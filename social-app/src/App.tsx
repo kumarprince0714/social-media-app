42a2dcfde9d3bf525718dfff1ccdab5a815f98dc
kumarprince1407 <kumarprince1407@gmail.com>
2025-02-23 08:21:00 +0530

    First commit - Initialized the project

---
+++ social-app/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="justify-center items-center text-blue-800">
+          Social App!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
